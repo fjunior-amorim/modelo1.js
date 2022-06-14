16687f49423e00289cddaf3c71b576d7b35c9f64
fjunior-amorim <86170296+fjunior-amorim@users.noreply.github.com>
2022-06-14 19:37:36 -0300

    atualização

--- script.js
+++ script.js
@@ -7,24 +7,24 @@ function horario() {
     if (hora >= 6 && hora < 12) {
         img.src = 'imagens/manha.png.jpg';
         document.body.style.background = '#e2cd9f';
-        msg.innerHTML = `Agora são ${hora} horas e ${minutos}minutos.<br>BOM DIA!`;
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br>BOM DIA!`;
     } else if (hora >= 12 && hora < 18) {
         img.src = 'imagens/meiodia.png.jpg';
         document.body.style.background = '#b9846f';
-        msg.innerHTML = `Agora são ${hora} horas e ${minutos}minutos.<br>BOA TARDE!`;
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br>BOA TARDE!`;
     } else if(hora > 18 && hora < 24) {
         img.src = 'imagens/noite.png.jpg';
         document.body.style.background = 'rgb(0, 27, 46)';
-        msg.innerHTML = `Agora são ${hora} horas e ${minutos}minutos.<br>BOA NOITE!`;
+        msg.innerHTML = `Agora são ${hora}:${minutos}<br>BOA NOITE!`;
     }else if(hora === 18) {
             img.src = 'imagens/pordosol.png.jpg';
             document.body.style.background = 'rgb(185, 50, 1)';
-            msg.innerHTML = `Agora são ${hora} horas e ${minutos}minutos.<br>FELIZ POR DO SOL!`;
+            msg.innerHTML = `Agora são ${hora}:${minutos}<br>FELIZ POR DO SOL!`;
     }else {
         if(hora === 1){
-            msg.innerHTML = `Agora são ${hora} hora e ${minutos}minutos.<br>TENHA UMA BOA MADRUGADA!`;
+            msg.innerHTML = `Agora são ${hora}:${minutos}<br>TENHA UMA BOA MADRUGADA!`;
         }else {
-            msg.innerHTML = `Agora são ${hora} horas e ${minutos}minutos.<br>TENHA UMA BOA MADRUGADA!`;
+            msg.innerHTML = `Agora são ${hora}:${minutos}<br>TENHA UMA BOA MADRUGADA!`;
         }
         img.src = 'imagens/madrugada.png.jpg';
         document.body.style.background = '#0c0c0c';
